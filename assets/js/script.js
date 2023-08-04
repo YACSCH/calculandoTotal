15b7d996f366b5575cbc72cbf0ff7f3be33bf0d5
Yamil Castillo <yamilcs@gmail.com>
2023-08-04 00:08:13 -0400

    Subiendo trabajo

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+let precio = document.querySelector(".precio").innerText
+let cantidad = document.querySelector("#txtcantidad")
+let color = document.querySelector("#txtcolor")
+let displayColor = document.querySelector(".card-result-circle")
+let displayPrice = document.querySelector(".card-result-total")
+let displayAmount = document.querySelector(".card-result-amount")
+
+
+function TotalCalculate() {
+    displayColor.style.backgroundColor = color.value;
+    displayPrice.innerText = precio * cantidad.value
+    displayAmount.innerText = cantidad.value
+}
